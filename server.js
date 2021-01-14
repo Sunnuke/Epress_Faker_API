a57a497b9fd0affb95345a0c9b661f5be4649d2f
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-13 22:39:15 -0800

    Created User class with fakers to randomly generate attribute values when created

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 import Express from "express";
 import Faker from "faker";
+
+class User {
+    constructor() {
+        this._id = Faker.random.number();
+        this.firstName = Faker.name.firstName();
+        this.lastName = Faker.name.lastName();
+        this.phoneNumber = Faker.phone.phoneNumber();
+        this.email = Faker.internet.email();
+        this.password = Faker.internet.password();
+    }
+}
+
