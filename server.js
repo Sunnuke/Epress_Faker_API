5e6a5d9ebaa292906e916ed3fd75c6501b8aea0f
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-13 23:26:53 -0800

    Updated API return methods to a fully functional format

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
 const express = require("express");
+const { company } = require("faker");
+const faker = require('faker');
 const app = express();
 
 class User {
     constructor() {
-        this._id = Faker.random.number();
-        this.firstName = Faker.name.firstName();
-        this.lastName = Faker.name.lastName();
-        this.phoneNumber = Faker.phone.phoneNumber();
-        this.email = Faker.internet.email();
-        this.password = Faker.internet.password();
+        this._id = faker.random.number();
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
     }
 }
 
 class Company {
     constructor() {
-        this._id = Faker.random.number();
-        this.name = Faker.company.companyName();
+        this._id = faker.random.number();
+        this.name = faker.company.companyName();
         this.address = {
-            street: Faker.address.streetName(),
-            city: Faker.address.city(),
-            state: Faker.address.state(),
-            zipCode: Faker.address.zipCodeByState(),
-            country: Faker.address.country()
+            street: faker.address.streetName(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCodeByState(),
+            country: faker.address.country()
         }
     }
 }
 
 
-app.get("/api/users/new", () => {
-    return console.log(new User());
+app.get("/api/users/new", (req, res) => {
+    res.send(new User());
 });
 
-app.get("/api/companies/new", () => {
-    return console.log(new Company());
+app.get("/api/companies/new", (req, res) => {
+    res.send(new Company());
 });
 
-app.get("/api/user/company", () => {
-    return console.log(new User(), new Company());
+app.get("/api/user/company", (req, res) => {
+    const test1 = new User();
+    const test2 = new Company();
+    const test3 = {
+        user: test1,
+        company: test2
+    }
+    res.send(test3);
 });
 
 app.listen( 8000, () => console.log(`Listening on port: ${8000}`) );
